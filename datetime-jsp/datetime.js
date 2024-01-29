af6dca58fa4a9dfc12b3fd191d5a61a2ce7289ce
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-01-28 18:39:45 -0600

    Format code

--- datetime-jsp/datetime.js
+++ datetime-jsp/datetime.js
@@ -7,16 +7,17 @@ var second = document.querySelector("#second");
 var millisecond = document.querySelector("#millisecond");
 var microsecond = document.querySelector("#microsecond");
 var nanosecond = document.querySelector("#nanosecond");
+
 function setCurrentDate() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
-    var currentMonth = currentDate.getMonth() + 1;//Add 1 to month because 0 is January in JavaScript and 1 in Java
+    var currentMonth = currentDate.getMonth() + 1; //Add 1 to month because 0 is January in JavaScript and 1 in Java
     var currentDay = currentDate.getDate();
     var currentHour = currentDate.getHours();
     var currentMinute = currentDate.getMinutes();
     var currentSecond = currentDate.getSeconds();
     var currentMillisecond = currentDate.getMilliseconds();
-    year.value = currentYear;//set the input values to current local date and time
+    year.value = currentYear; //set the input values to current local date and time
     month.value = currentMonth;
     day.value = currentDay;
     hour.value = currentHour;
@@ -26,6 +27,7 @@ function setCurrentDate() {
     microsecond.value = 0;
     nanosecond.value = 0;
 }
+
 function setCurrentUtcDate() {
     var currentDate = new Date();
     var currentUtcYear = currentDate.getUTCFullYear();
@@ -35,7 +37,7 @@ function setCurrentUtcDate() {
     var currentUtcMinute = currentDate.getUTCMinutes();
     var currentUtcSecond = currentDate.getUTCSeconds();
     var currentUtcMillisecond = currentDate.getUTCMilliseconds();
-    year.value = currentUtcYear;//set the input values to current UTC date and time
+    year.value = currentUtcYear; //set the input values to current UTC date and time
     month.value = currentUtcMonth;
     day.value = currentUtcDay;
     hour.value = currentUtcHour;
@@ -45,6 +47,7 @@ function setCurrentUtcDate() {
     microsecond.value = 0;
     nanosecond.value = 0;
 }
+
 function getDaysInMonth() {
     day.max = new Date(year.value, month.value, 0).getDate();
     if (day.value > day.max) {
