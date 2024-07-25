e0899599e71815f266a74291c682b393442c1709
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-07-25 10:52:35 -0500

    Format code

--- datetime-thymeleaf/src/main/java/com/example/lulunac27a/datetimethymeleaf/entity/DateTime.java
+++ datetime-thymeleaf/src/main/java/com/example/lulunac27a/datetimethymeleaf/entity/DateTime.java
@@ -1,6 +1,7 @@
 package com.example.lulunac27a.datetimethymeleaf.entity;
 
-public class DateTime {//class with date and time information with milliseconds, microseconds and nanoseconds
+public class DateTime {// class with date and time information with milliseconds, microseconds and
+                       // nanoseconds
     private int year;
     private int month;
     private int day;
@@ -10,60 +11,78 @@ public class DateTime {//class with date and time information with milliseconds,
     private int millisecond;
     private int microsecond;
     private int nanosecond;
-    //get getter and setter values for all fields
+
+    // get getter and setter values for all fields
     public int getYear() {
         return year;
     }
+
     public void setYear(int year) {
         this.year = year;
     }
+
     public int getMonth() {
         return month;
     }
+
     public void setMonth(int month) {
         this.month = month;
     }
+
     public int getDay() {
         return day;
     }
+
     public void setDay(int day) {
         this.day = day;
     }
+
     public int getHour() {
         return hour;
     }
+
     public void setHour(int hour) {
         this.hour = hour;
     }
+
     public int getMinute() {
         return minute;
     }
+
     public void setMinute(int minute) {
         this.minute = minute;
     }
+
     public int getSecond() {
         return second;
     }
+
     public void setSecond(int second) {
         this.second = second;
     }
+
     public int getMillisecond() {
         return millisecond;
     }
+
     public void setMillisecond(int millisecond) {
         this.millisecond = millisecond;
     }
+
     public int getMicrosecond() {
         return microsecond;
     }
+
     public void setMicrosecond(int microsecond) {
         this.microsecond = microsecond;
     }
+
     public int getNanosecond() {
         return nanosecond;
     }
+
     public void setNanosecond(int nanosecond) {
         this.nanosecond = nanosecond;
     }
-    
+
 }
